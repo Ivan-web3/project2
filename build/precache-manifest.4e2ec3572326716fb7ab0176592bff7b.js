8df161ab477a3431be0e20db0b0b4e5799771cf0
Ivan <vanya-99@list.ru>
2020-04-29 13:47:39 +0300

    build

---
+++ build/precache-manifest.4e2ec3572326716fb7ab0176592bff7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "96def03031c6178fa8ca",
+    "url": "/static/css/main.fa057a08.chunk.css"
+  },
+  {
+    "revision": "96def03031c6178fa8ca",
+    "url": "/static/js/main.96def030.chunk.js"
+  },
+  {
+    "revision": "2af167fd786967b5153a",
+    "url": "/static/css/1.2e33211f.chunk.css"
+  },
+  {
+    "revision": "2af167fd786967b5153a",
+    "url": "/static/js/1.2af167fd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "4946c4ec81f5c46a95a105a2f49d1895",
+    "url": "/index.html"
+  }
+];
